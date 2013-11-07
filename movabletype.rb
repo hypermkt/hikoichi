c89f9774f67222d86f51cd47997d784fa506649e
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2013-11-07 10:12:39 +0900

    インデント幅を2にしました
      see1: http://www.ruby.or.jp/ja/tech/development/ruby/050_coding_rule.html
      see2: https://github.com/styleguide/ruby

--- movabletype.rb
+++ movabletype.rb
@@ -1,28 +1,28 @@
 class MovableType
-    def initialize(package)
-        @package = package
-    end
+  def initialize(package)
+    @package = package
+  end
 
-    def check
-        page = open(@package['feed_url'])
-        rss = SimpleRSS.parse(page)
-        file = File.read(CHECK_FILE)
-        list = Array.new
-        rss.entries.each do|item|
-            url = item.link
-            if !file.include?(url) &&
-                item.title.force_encoding("UTF-8").include?('リリース')
-                File.open(CHECK_FILE,"a"){|f|
-                    f.puts url
-                }
-                message = sprintf("[%s] %s %s %s",
-                                  @package['name'],
-                                  item.published.strftime("%Y/%m/%d"),
-                                  item.title,
-                                  item.link)
-                list.push(message)
-            end
-        end
-        return list
+  def check
+    page = open(@package['feed_url'])
+    rss = SimpleRSS.parse(page)
+    file = File.read(CHECK_FILE)
+    list = Array.new
+    rss.entries.each do|item|
+      url = item.link
+      if !file.include?(url) &&
+        item.title.force_encoding("UTF-8").include?('リリース')
+        File.open(CHECK_FILE,"a"){|f|
+          f.puts url
+        }
+        message = sprintf("[%s] %s %s %s",
+                          @package['name'],
+                          item.published.strftime("%Y/%m/%d"),
+                          item.title,
+                          item.link)
+        list.push(message)
+      end
     end
+    return list
+  end
 end
