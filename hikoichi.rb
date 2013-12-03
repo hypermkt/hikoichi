b34a40ba7fc59c631121aa9e6ba4c304318f8801
Makoto Chiba <hypermkt@paperboy.co.jp>
2013-12-03 21:30:08 +0900

    ikachanでPOSTするようにした

--- hikoichi.rb
+++ hikoichi.rb
@@ -5,6 +5,7 @@ require 'open-uri'
 require 'yaml'
 require 'nokogiri'
 require 'yaml'
+require 'net/http'
 
 require_relative './wordpress.rb'
 require_relative './movabletype.rb'
@@ -14,12 +15,32 @@ require_relative './mediawiki.rb'
 
 CHECK_FILE = 'checked_list.txt'
 
-yaml = YAML.load_file "packages.yaml"
-yaml['packages'].each do |package|
-  mod = Module.new
-  cms = mod.const_get(package['class']).new(package)
-  list = cms.check
-  list.each do|message|
-    # TODO: call ikachan
+class Hikoichi
+  def initialize
+    @packages = YAML.load_file "packages.yaml"
+    @config = YAML.load_file "config.yaml"
+  end
+
+  def ikachan_post(message)
+    Net::HTTP.post_form(URI.parse(@config['irc']['host']), {
+      'channel' => @config['irc']['channel'],
+      'message' => message,
+    })
+  end
+
+  def check
+    ikachan_post('要チェック〜！')
+    @packages['packages'].each do |package|
+      mod = Module.new
+      cms = mod.const_get(package['class']).new(package)
+      list = cms.check
+      list.each do|message|
+        ikachan_post(message)
+      end
+    end
+    ikachan_post('終わり！')
   end
 end
+
+hikoichi = Hikoichi.new
+hikoichi.check
